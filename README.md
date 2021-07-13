0968c0536e3360948378fd9bcba0ee0a4f73aa3f
IbzanCastaDiaz <ibzan.lml@gmail.com>
2021-07-12 23:36:07 -0600

    Figuras geometricas

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # js-practico
 Curso JavaScrip Practicos. Notas y ejercicios
+
+## Taller #1: figutas geométricas
+
+-Primer paso: definir las fórmulas
+-Segundo paso: implentar la fórmulas en JavaScript
+Tercer paso: crear funciones
+Cuarto paso: integrar JS con HTML
+
---
+++ figuras.js
@@ -0,0 +1,24 @@
+//Código del cuadrado
+
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado mide: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
+console.groupEnd();
+
+//Código del triangulo
+console.group("Tríangulos");
+const ladoTriangulo1= 6;
+const ladoTriangulo2= 6;
+const baseTriangulo= 4;
+
+console.log("Los lados del tríangulo son: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
+console.groupEnd();
+
+
+
